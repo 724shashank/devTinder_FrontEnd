067f0fdfc4b4a7feb4f4c683c8d51345c39afac2
Shashank Yadav <shashankyadav724@gmail.com>
2025-10-19 23:55:43 +0530

    changes

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,10 +4,20 @@ import { baseURL } from "./constants";
 let socket;
 
 export const createSocketConnection = () => {
-  if(!socket){
-    socket = io(baseURL, {
-    withCredentials: true, // send cookies
-  });
+  if (!socket) {
+    if (baseURL === "http://localhost:3000") {
+      // Local development
+      socket = io(baseURL, {
+        withCredentials: true, // send cookies
+      });
+    } else {
+      // Production (e.g., deployed on Render + Vercel)
+      socket = io("/", {
+        path: "/api/socket.io",
+        withCredentials: true,
+      });
+    }
   }
-   return socket ;
+
+  return socket;
 };
